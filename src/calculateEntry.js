dc646b9c44ce0349e3923b37e67ebf1d8d158db9
Ester Viana Verza <ester.av@hotmail.com>
2022-07-28 19:08:08 -0300

    requisito 1

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -2,7 +2,7 @@ const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
   const try1 = 10;
-  console.log (try1)
+  console.log(try1);
 }
 
 function calculateEntry(entrants) {
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  const animal = species.filter((searchedAnimal) => ids.includes(searchedAnimal.id));
+  return animal;
 }
+
+console.log(getSpeciesByIds('e8481c1d-42ea-4610-8e11-1752cfc05a46'));
 
 module.exports = getSpeciesByIds;
