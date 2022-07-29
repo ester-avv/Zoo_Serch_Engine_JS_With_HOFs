4aec5ce83351a3bb6cbcb87945fa40e4ec5676cf
Ester Viana Verza <ester.av@hotmail.com>
2022-07-29 11:18:44 -0300

    insere requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,9 +1,9 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
- const chosenAnimal = data.species.find((myAnimal) => myAnimal.name === animal);
- const animalsOlderThan = chosenAnimal.residents.every((sameSpAnimal) => sameSpAnimal.age >= age);
- return animalsOlderThan; 
+  const chosenAnimal = data.species.find((myAnimal) => myAnimal.name === animal);
+  const animalsOlderThan = chosenAnimal.residents.every((sameSpAnimal) => sameSpAnimal.age >= age);
+  return animalsOlderThan;
 }
 
 console.log(getAnimalsOlderThan('tigers', 10));
