6e9e34fae128a4f8faf70b4aaa2d949510dba373
Ester Viana Verza <ester.av@hotmail.com>
2022-07-29 13:53:09 -0300

    insere função getEmployeeByName

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,18 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (!employeeName) { return {}; }
+  const employee = data.employees.filter((searchedEmployee) => {
+    if (searchedEmployee.firstName === employeeName) {
+      return searchedEmployee;
+    } if (searchedEmployee.lastName === employeeName) {
+      return searchedEmployee;
+    }
+    return searchedEmployee;
+  });
+  return employee[0];
 }
+
+console.log(getEmployeeByName());
 
 module.exports = getEmployeeByName;
