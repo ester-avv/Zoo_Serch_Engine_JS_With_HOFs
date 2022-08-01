26fb20eaf8b73699c5ce95fe15c8eda433d398c7
Ester Viana Verza <ester.av@hotmail.com>
2022-08-01 19:02:15 -0300

    Insere função countAnimals

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,26 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    const allAnimals = data.species.reduce((acc, currentSpecie) => {
+      acc[currentSpecie.name] = currentSpecie.residents.length;
+      return acc;
+    }, {});
+    return allAnimals;
+  } if (Object.keys(animal).length === 1) {
+    const searchedAnimal = species.find((selectedSpecie) =>
+      selectedSpecie.name === animal.specie);
+    return searchedAnimal.residents.length;
+  } if (Object.keys(animal).length === 2) {
+    const searchedAnimal = species.find((selectedSpecie) =>
+      selectedSpecie.name === animal.specie);
+    const residentsAnimals = searchedAnimal.residents.filter((resident) =>
+      resident.sex === animal.sex);
+    return residentsAnimals.length;
+  }
 }
+
+console.log(countAnimals({ specie: 'bears', sex: 'female' }));
 
 module.exports = countAnimals;
